9a6f1b97be0869c82c35fa3b5de0e822e9dd1cfd
Fasalsan <Sanfasal70@gmail.com>
2025-08-08 14:23:40 +0700

    update text color

--- app/components/Card.js
+++ app/components/Card.js
@@ -4,11 +4,11 @@ import Link from 'next/link';
 export default function Card({ icon: Icon, title, count, href }) {
     return (
         <Link href={href}>
-            <div className="w-full sm:w-64 md:w-72 lg:w-100 p-6 sm:p-9 bg-white rounded-2xl border border-pink-100 shadow-sm text-center hover:shadow-xl transition-all duration-500 group cursor-pointer">
+            <div className="w-full sm:w-64 md:w-72 lg:w-100 p-6 sm:p-9 bg-white rounded-2xl border border-blue-100 shadow-sm text-center hover:shadow-xl transition-all duration-500 group cursor-pointer">
                 <div className="text-pink-600 text-4xl mb-4 transition-transform duration-500 group-hover:rotate-[360deg]">
                     <Icon className="inline-block" />
                 </div>
-                <h2 className="text-xl font-semibold mb-2">{title}</h2>
+                <h2 className="text-xl font-semibold mb-2 text-black">{title}</h2>
                 <p className="text-gray-500">{count}</p>
             </div>
         </Link>
